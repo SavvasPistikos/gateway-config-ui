0a1ce50dddbea56d1164bcd1615b66c27ac9202f
pistikos <pistikos@intralot.com>
2020-06-02 17:54:25 +0300

    progress to add Paths....

--- src/component/SwaggePath.js
+++ src/component/SwaggePath.js
@@ -1,10 +1,14 @@
 import React from "react";
-import Accordion from "react-bootstrap/Accordion";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
 
 class SwaggerPath extends React.Component {
-  state = {};
+  state = { checked: false };
+
+  handleChange = () => {
+    this.setState((prev) => {
+      return { checked: !prev.checked };
+    });
+    this.props.handlePath(this);
+  };
 
   render() {
     let httpmethods = [];
@@ -14,18 +18,16 @@ class SwaggerPath extends React.Component {
 
     return httpmethods.map((m) => {
       return (
-        <Accordion defaultActiveKey="0">
-          <Card>
-            <Card.Header>
-              <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                {m} {this.props.url}
-              </Accordion.Toggle>
-            </Card.Header>
-            <Accordion.Collapse eventKey="0">
-              <Card.Body>Hello! I'm the body</Card.Body>
-            </Accordion.Collapse>
-          </Card>
-        </Accordion>
+        <div>
+          <p>
+            {m} {this.props.url}
+            <input
+              type="checkbox"
+              checked={this.state.checked}
+              onChange={this.handleChange}
+            ></input>
+          </p>
+        </div>
       );
     });
   }
--- src/component/SwaggerApi.js
+++ src/component/SwaggerApi.js
@@ -2,11 +2,27 @@ import React from "react";
 import SwaggerPath from "./SwaggePath.js";
 
 class SwaggerApi extends React.Component {
+  state = { paths: [] };
+
+  handlePath = (path) => {
+    if (!path.state.checked) {
+      this.setState((prev) => {
+        const paths = [...this.state.paths, path.props.url];
+        return {
+          paths,
+        };
+      });
+    }
+  };
   render() {
     return (
       <div>
         {Object.keys(this.props.api.paths).map((s) => (
-          <SwaggerPath url={s} path={this.props.api.paths[s]} />
+          <SwaggerPath
+            url={s}
+            path={this.props.api.paths[s]}
+            handlePath={this.handlePath}
+          />
         ))}
       </div>
     );
